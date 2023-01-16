a527c897fd19ebafe65bc6efacbecfd3a72e8413
Bryan Hite <bhite2@mail.usf.edu>
2023-01-16 10:41:26 -0500

    Pushing minor changes made to update

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarsSerializer
+from .models import Cars
 
-@api_view(["GET"])
+@api_view(['GET', 'POST'])  
 def cars_list(request):
     
-    
-    return Response(["ok"])
+    if request.method == 'GET':
+        cars = Cars.objects.all()
+        serializer = CarsSerializer(cars, many=True)
+        return Response(serializer.data)
+    elif request.method == 'Post':
+        pass
